589193854c866acf63abebfc3f3e7e76c901ae86
Shoaib <shoaibchemistrysust@gmail.com>
2025-06-08 20:38:42 +0600

    a database name crowdfundingDB was created in MongoDB atlas

--- index.js
+++ index.js
@@ -28,17 +28,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    const database = client.db("crowdfundingDB");
+    const usersCollection = database.collection("users");
+
+    // post method: for creating users
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log('New User:', user);
+      try {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (err) {
+        console.error('Insert error:', err);
+        res.status(500).send({ message: 'Failed to insert user', error: err });
+      }
+    });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
   }
+  
 }
 run().catch(console.dir);
+
 
 
 
