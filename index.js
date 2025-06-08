feaeb28dc4ef37c809ec35600da5d071235efaf9
Shoaib <shoaibchemistrysust@gmail.com>
2025-06-08 22:18:06 +0600

    POST method used applied to create campaign in MongoDB

--- index.js
+++ index.js
@@ -31,13 +31,28 @@ async function run() {
     await client.connect();
     const database = client.db("crowdfundingDB");
     const usersCollection = database.collection("users");
-
     // post method: for creating users
     app.post('/users', async (req, res) => {
       const user = req.body;
       console.log('New User:', user);
       try {
         const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (err) {
+        console.error('Insert error:', err);
+        res.status(500).send({ message: 'Failed to insert user', error: err });
+      }
+    });  
+
+
+     
+    const campaignsCollection = database.collection("campaigns");
+    // post method: for creating campaigns
+    app.post('/addCampaign', async (req, res) => {
+      const campaign = req.body;
+      console.log('New Campaign:', campaign);
+      try {
+        const result = await campaignsCollection.insertOne(campaign);
         res.send(result);
       } catch (err) {
         console.error('Insert error:', err);
