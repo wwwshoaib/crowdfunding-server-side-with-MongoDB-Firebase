0e6b91eeb1c6580cdeecf24b3ae01ceccf00ca2d
Shoaib <shoaibchemistrysust@gmail.com>
2025-06-18 22:47:03 +0600

    Delete Operation method was implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -67,6 +67,15 @@ async function run() {
         res.send(result);
      
     });
+
+    //Delete method: for deleting a campaign from my campaigns
+
+    app.delete('/addCampaign/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await campaignsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
